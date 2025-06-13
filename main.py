da77a8edb728dac426a787e64af8987e9239c74d
Akash-Balaji003 <akashbalaji.tvr@gmail.com>
2025-06-14 01:48:44 +0530

    Used the simple_icd_10 library to get all ICD codes and converted it to excel format.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# Web Scraping yet to bo done
+import simple_icd_10 as icd
+import pandas as pd
+
+# Initialize the ICD-10 codes to store in the database
+all_codes = icd.get_all_codes()
+
+# Prepare a list of dicts with code and description
+data = [{"Code": code, "Description": icd.get_description(code)} for code in all_codes]
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+
+# Save to Excel
+df.to_excel("icd10cm_codes.xlsx", index=False)
+
+print("ICD-10-CM codes exported to 'icd10cm_codes.xlsx'")
